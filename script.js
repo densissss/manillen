55a6c93cf766964480aac3ba849094b9ad266df8
densissss <77514867+densissss@users.noreply.github.com>
2023-12-12 11:23:58 +0100

    optellen werkt

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function opschrijvenLinks() {
-    var scoreNieuw = document.getElementById("puntenl").value;
-    var scoreOud = document.getElementsByClassName("scoreslinksStrike")[0];
-    var scoreTotaal = document.getElementsByClassName("scoreslinks")[0]
-    scoreOud.innerHTML = scoreOud.innerHTML + "<br>" + scoreTotaal.innerHTML;
-    let scoreLijst = [0];
-    scoreOud.classList.add("strike"); 
-    scoreTotaal.innerHTML = scoreNieuw;
-    scoreLijst.push(scoreNieuw)
+    var scoreNieuwL = parseInt(document.getElementById("puntenl").value);
+    var scoreOudL = document.getElementsByClassName("scoresOudLinks")[0];
+    var scoreTotaalL = document.getElementById("totaalLinks");
+    scoreOudL.innerHTML = scoreOudL.innerHTML + "<br>" + scoreTotaalL.innerHTML;
+    scoreOudL.classList.add("strike");
+    scoreTotaalL.innerHTML = parseInt(scoreTotaalL.innerHTML) + scoreNieuwL;
+}
 
-
+function opschrijvenRechts() {
+    var scoreNieuwR = parseInt(document.getElementById("puntenr").value);
+    var scoreOudR = document.getElementsByClassName("scoresOudRechts")[0];
+    var scoreTotaalR = document.getElementById("totaalRechts");
+    scoreOudR.innerHTML = scoreOudR.innerHTML + "<br>" + scoreTotaalR.innerHTML;
+    scoreOudR.classList.add("strike");
+    scoreTotaalR.innerHTML = parseInt(scoreTotaalR.innerHTML) + scoreNieuwR;
 }
