6c09d2a40966183a5c48339f4011efad93b3a370
densissss <77514867+densissss@users.noreply.github.com>
2023-12-12 14:53:05 +0100

    scores werken volledig

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+
+const scoreLijstL = [0];
+const scoreLijstR = [0];
+
 function opschrijvenLinks() {
     var scoreNieuwL = parseInt(document.getElementById("puntenl").value);
-    var scoreOudL = document.getElementsByClassName("scoresOudLinks")[0];
+    var scoreOudL = document.getElementById("scoresOudLinks");
     var scoreTotaalL = document.getElementById("totaalLinks");
-    scoreOudL.innerHTML = scoreOudL.innerHTML + "<br>" + scoreTotaalL.innerHTML;
+    scoreLijstL.push(parseInt(scoreTotaalL.innerHTML) + scoreNieuwL); 
+    scoreOudL.innerHTML = scoreLijstL.slice(0, scoreLijstL.length - 1).join('<br>');
     scoreOudL.classList.add("strike");
     scoreTotaalL.innerHTML = parseInt(scoreTotaalL.innerHTML) + scoreNieuwL;
 }
 
 function opschrijvenRechts() {
     var scoreNieuwR = parseInt(document.getElementById("puntenr").value);
-    var scoreOudR = document.getElementsByClassName("scoresOudRechts")[0];
+    var scoreOudR = document.getElementById("scoresOudRechts");
     var scoreTotaalR = document.getElementById("totaalRechts");
-    scoreOudR.innerHTML = scoreOudR.innerHTML + "<br>" + scoreTotaalR.innerHTML;
+    scoreLijstR.push(parseInt(scoreTotaalR.innerHTML) + scoreNieuwR); 
+    scoreOudR.innerHTML = scoreLijstR.slice(0, scoreLijstR.length - 1).join('<br>');
     scoreOudR.classList.add("strike");
     scoreTotaalR.innerHTML = parseInt(scoreTotaalR.innerHTML) + scoreNieuwR;
 }
+
+//function undoLinks() {
+//    scoreTotaalL.innerHTML.pop();
+//}
